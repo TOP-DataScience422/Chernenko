a7ba81afcbe2a8805636eb5b37a388805aa22986
Lelik153 <freshmine@yandex.ru>
2024-04-20 14:37:58 +0800

    Домашняя работа

---
+++ 2024.04.07/1.1.py
@@ -0,0 +1,18 @@
+name = input ("Введите имя: ")
+print ("\n")
+family = input ("Введите фамилию: ")
+print ("\n")
+age = int(input ("Введите год рождения: "))
+print ("\n")
+year = 2024
+age1 = (year - age)
+print ("\n")
+print (family, name,",", age1)
+
+
+# Введите фамилию: Черненко
+
+# Введите год рождения: 1987
+
+
+# Черненко Ольга , 37
